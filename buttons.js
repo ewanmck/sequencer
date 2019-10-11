f0487b90ef93b028e96d8019246b89166f89ca3f
Ewan McKinley <ewan@ewans-mbp.wireless-psk.unc.edu>
2019-10-11 10:12:31 -0400

    first commit of page and button handler

---
+++ buttons.js
@@ -0,0 +1,184 @@
+//button handlers
+
+let track1Toggles = {
+    "1" : false,
+    "2" : false,
+    "3" : false,
+    "4" : false,
+    "5" : false,
+    "6" : false,
+    "7" : false,
+    "8" : false,
+    "9" : false,
+    "10" : false,
+    "11" : false,
+    "12" : false,
+    "13" : false,
+    "14" : false,
+    "15" : false,
+    "16" : false,
+}
+
+let track2Toggles = {
+    "1" : false,
+    "2" : false,
+    "3" : false,
+    "4" : false,
+    "5" : false,
+    "6" : false,
+    "7" : false,
+    "8" : false,
+    "9" : false,
+    "10" : false,
+    "11" : false,
+    "12" : false,
+    "13" : false,
+    "14" : false,
+    "15" : false,
+    "16" : false,
+}
+
+let track3Toggles = {
+    "1" : false,
+    "2" : false,
+    "3" : false,
+    "4" : false,
+    "5" : false,
+    "6" : false,
+    "7" : false,
+    "8" : false,
+    "9" : false,
+    "10" : false,
+    "11" : false,
+    "12" : false,
+    "13" : false,
+    "14" : false,
+    "15" : false,
+    "16" : false,
+}
+
+let track4Toggles = {
+    "1" : false,
+    "2" : false,
+    "3" : false,
+    "4" : false,
+    "5" : false,
+    "6" : false,
+    "7" : false,
+    "8" : false,
+    "9" : false,
+    "10" : false,
+    "11" : false,
+    "12" : false,
+    "13" : false,
+    "14" : false,
+    "15" : false,
+    "16" : false,
+}
+
+let track5Toggles = {
+    "1" : false,
+    "2" : false,
+    "3" : false,
+    "4" : false,
+    "5" : false,
+    "6" : false,
+    "7" : false,
+    "8" : false,
+    "9" : false,
+    "10" : false,
+    "11" : false,
+    "12" : false,
+    "13" : false,
+    "14" : false,
+    "15" : false,
+    "16" : false,
+}
+
+let track6Toggles = {
+    "1" : false,
+    "2" : false,
+    "3" : false,
+    "4" : false,
+    "5" : false,
+    "6" : false,
+    "7" : false,
+    "8" : false,
+    "9" : false,
+    "10" : false,
+    "11" : false,
+    "12" : false,
+    "13" : false,
+    "14" : false,
+    "15" : false,
+    "16" : false,
+}
+
+let track7Toggles = {
+    "1" : false,
+    "2" : false,
+    "3" : false,
+    "4" : false,
+    "5" : false,
+    "6" : false,
+    "7" : false,
+    "8" : false,
+    "9" : false,
+    "10" : false,
+    "11" : false,
+    "12" : false,
+    "13" : false,
+    "14" : false,
+    "15" : false,
+    "16" : false,
+}
+
+let track8Toggles = {
+    "1" : false,
+    "2" : false,
+    "3" : false,
+    "4" : false,
+    "5" : false,
+    "6" : false,
+    "7" : false,
+    "8" : false,
+    "9" : false,
+    "10" : false,
+    "11" : false,
+    "12" : false,
+    "13" : false,
+    "14" : false,
+    "15" : false,
+    "16" : false,
+}
+
+let tracks = {
+    "track1" : track1Toggles,
+    "track2" : track2Toggles,
+    "track3" : track3Toggles,
+    "track4" : track4Toggles,
+    "track5" : track5Toggles,
+    "track6" : track6Toggles,
+    "track7" : track7Toggles,
+    "track8" : track8Toggles,
+}
+
+//Toggles the button in css and toggles the value in the set of dictionaries
+const toggleButtons= function(event) {
+    let button = $(event.target);
+
+    button.toggleClass("is-grey");
+    button.toggleClass("is-warning");
+
+    let trackID = button.parent().parent().parent().attr("id");
+    let buttonNum = button.text();
+    tracks[trackID][buttonNum] = !tracks[trackID][buttonNum];
+};
+
+function registerButtonHandler() {
+    $(".button").on("click", toggleButtons);
+}
+
+$(function() {
+   registerButtonHandler();
+});
