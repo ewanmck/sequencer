e2de67ce00bf0a7883c8795f011c60d06e061606
Ewan McKinley <ewan@live.unc.edu>
2019-12-06 10:40:36 -0500

    added more samples

--- library.js
+++ library.js
@@ -89,22 +89,48 @@ $(function() {
 
 let libraryData = {
     "kick": {
-        "SPV3_KICK2" : "samples/SPV3_KICK2 fav.wav"
+        "SPV3_KICK2" : "samples/SPV3_KICK2 fav.wav",
+        "atom_kick" : "samples/atom_kick.wav",
+        "EkaliBlessingsKick" : "samples/EkaliBlessingsKick.wav",
     },
+    "bass" :{
+        "disorted moog bass" : "samples/disorted moog bass.wav",
+        "sam g's 808" : "samples/sam g's 808.wav",
+        "slide808" : "samples/slide808.wav",
+        "us 808" : "samples/us 808.wav",
+    }, 
     "snare": {
-        "Jack Ü" : "samples/Jack Ü - Take Ü There Snare.wav"
+        "Jack Ü" : "samples/Jack Ü - Take Ü There Snare.wav",
+        "CLAP_BORN" : "samples/CLAP_BORN.wav",
+        "Fed up slap" : "samples/Fed up slap.wav",
+        "OrphansChopSnare" : "samples/OrphansChopSnare.wav",
     },
     "hats": {
-        "hy_hat" : "samples/hy_hat.wav"
+        "hy_hat" : "samples/hy_hat.wav",
+        "flash-3" : "samples/flash-3.mp3",
+        "shaker (5)" : "samples/shaker (5).wav",
     },
     "tom": {
         "SPV3_TOM2" : "samples/SPV3_TOM2.wav"
     },
     "synth": {
-        "synth 3" : "samples/synth 3.wav"
+        "synth 3" : "samples/synth 3.wav",
+        "Chop (3)" : "samples/Chop (3).wav",
+        "EM_Synth Chord 6 (Dmin7)" : "samples/EM_Synth Chord 6 (Dmin7).wav",
+        "Synth (8)" : "samples/Synth (8).wav"
     },
     "sample": {
-        "AiryFlute_01_SP" : "samples/AiryFlute_01_SP.wav"
+        "AiryFlute_01_SP" : "samples/AiryFlute_01_SP.wav",
+        "A_KotoStab_SP" : "samples/A_KotoStab_SP.wav",
+        "C_LiveRhodes_SP" : "samples/C_LiveRhodes_SP.wav",
+        "e_fat" : "samples/e_fat.wav",
+        "F_SoulStab_SP" : "samples/F_SoulStab_SP.wav",
     },
-    "fx": {},
+    "fx": {
+        "Big Pot 5" : "samples/Big Pot 5.wav",
+        "Coins on a Skin b" : "samples/Coins on a Skin b.wav",
+        "perc 3" : "samples/perc 3.wav",
+        "vox 5" : "samples/vox 5.wav",
+        "VOX 6" : "samples/VOX 6.wav"
+    },
 };
